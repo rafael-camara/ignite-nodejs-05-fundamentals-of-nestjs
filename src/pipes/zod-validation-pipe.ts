ea83270cc6cc0a62c2628689e490191362b88bd5
rafael-camara <rafaelcamara.377@gmail.com>
2025-09-27 23:28:15 -0300

    feat: creating zod validation pipe

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,24 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common'
+import { z } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: z.ZodType) {}
+
+  transform(value: unknown) {
+    try {
+      const parsedValue = this.schema.parse(value)
+      return parsedValue
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        throw new BadRequestException({
+          message: 'Validation failed',
+          statusCode: 400,
+          errors: fromZodError(error),
+        })
+      }
+
+      throw new BadRequestException('Validation failed')
+    }
+  }
+}
