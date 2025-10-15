3d14505444bdd7aa1b4112372fe3cad7f25c44be
rafael-camara <rafaelcamara.377@gmail.com>
2025-10-15 10:33:39 -0300

    feat: creating EnvModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+
 import { AuthModule } from './auth/auth.module'
-import { envSchema } from './env'
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
 import { HttpModule } from './http/http.module'
 
 @Module({
@@ -12,6 +14,7 @@ import { HttpModule } from './http/http.module'
     }),
     AuthModule,
     HttpModule,
+    EnvModule,
   ],
 })
 export class AppModule {}
