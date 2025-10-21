9c751b75af71647b2f53ed718c4d79a410b3425a
rafael-camara <rafaelcamara.377@gmail.com>
2025-10-21 10:49:42 -0300

    feat: edit answer controller

---
+++ src/infra/http/controllers/edit-answer.controller.ts
@@ -0,0 +1,48 @@
+import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+import * as jwtStrategy from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Put,
+} from '@nestjs/common'
+import z from 'zod'
+
+const editAnswerBodySchema = z.object({
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(editAnswerBodySchema)
+
+type EditAnswerBodySchema = z.infer<typeof editAnswerBodySchema>
+
+@Controller('/answers/:id')
+export class EditAnswerController {
+  constructor(private editAnswerUseCase: EditAnswerUseCase) {}
+
+  @Put()
+  @HttpCode(204)
+  async handle(
+    @Body(bodyValidationPipe) body: EditAnswerBodySchema,
+    @CurrentUser() user: jwtStrategy.UserPayload,
+    @Param('id') answerId: string,
+  ) {
+    const { content } = body
+    const { sub: userId } = user
+
+    const result = await this.editAnswerUseCase.execute({
+      content,
+      answerId,
+      authorId: userId,
+      attachmentsIds: [],
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,6 +4,7 @@ import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answ
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
+import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer'
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
@@ -15,6 +16,7 @@ import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { DeleteQuestionController } from './controllers/delete-question.controller'
+import { EditAnswerController } from './controllers/edit-answer.controller'
 import { EditQuestionController } from './controllers/edit-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
@@ -30,6 +32,7 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     EditQuestionController,
     DeleteQuestionController,
     AnswerQuestionController,
+    EditAnswerController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -40,6 +43,7 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     EditQuestionUseCase,
     DeleteQuestionUseCase,
     AnswerQuestionUseCase,
+    EditAnswerUseCase,
   ],
 })
 export class HttpModule {}
